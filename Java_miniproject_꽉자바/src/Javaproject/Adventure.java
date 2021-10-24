c93bab5e22bbeb26125344b39e45f212e0756932
smhrd <smhrd@DESKTOP-L0VQ6TC>
2021-10-24 13:19:38 +0900

    당신은 사망하셨습니다.

--- Java_miniproject_꽉자바/src/Javaproject/Adventure.java
+++ Java_miniproject_꽉자바/src/Javaproject/Adventure.java
@@ -7,13 +7,14 @@ public class Adventure {
 	Scanner sc= new Scanner(System.in);
 	Battle bt = new Battle();
 	Eat ea = new Eat();
+	DAMAVO st=new DAMAVO();
 	int select_admenu;
 	
 	public void select_Adven(String main_id) {
 		select_admenu=0;
 		while (true) {
 			if (select_admenu==0) {
-			System.out.println("[1]사냥하기 [2]음식구하기 [3]약구하기");
+			System.out.println("[1]사냥하기 [2]음식구하기 [3]약구하기 [4]뒤로가기");
 			select_admenu=sc.nextInt();
 			}else if(select_admenu==1) {
 				System.out.println("[1]핓빛숲 [2]심해 [3]폭풍정원");
@@ -29,6 +30,8 @@ public class Adventure {
 				ea.Eat_save(main_id);
 				break;
 				
+			}else if(select_admenu==4) {
+				break;
 			}else {
 				select_admenu=0;
 			}
