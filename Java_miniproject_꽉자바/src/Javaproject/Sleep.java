31363b47676660877047a875ade0a15c05b60bb7
smhrd <smhrd@DESKTOP-1HHI0S3>
2021-10-24 23:38:50 +0900

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of
    https://github.com/2021-SMHRD-KDT-Bigdata-7/Javaminiproject1.git

--- Java_miniproject_꽉자바/src/Javaproject/Sleep.java
+++ Java_miniproject_꽉자바/src/Javaproject/Sleep.java
@@ -10,24 +10,18 @@ public class Sleep {
 	int result = 0;
 	
 	DAMADAO ddao = new DAMADAO();
+	DAMAVO dvo = new DAMAVO();
+	Face fc = new Face();
+	
+	private int hp;
+	private int ene;
+	private int maxhp;
+	private int maxene;
+	private int startday;
+	 	
 	
 	public int sleep(String id) {
-			ddao.getConn();
-			try {
-				// 수정할 컬럼이 한개 이상일 경우 set 컬럼1 = ?, 컬럼2 = ?, ...
-				String sql = "update dama_info set hp = 100, ene = 100, startday = startday + 1 where id = ?";
-				
-				ddao.psmt = ddao.conn.prepareStatement(sql);
-				ddao.psmt.setString(1, id);
-				
-				result = ddao.psmt.executeUpdate();
-				
-			}catch(Exception e) {
-				e.printStackTrace();
-			}finally {
-				ddao.endClose();
-			}
-			return result;
+			
 		}
 	
 }
