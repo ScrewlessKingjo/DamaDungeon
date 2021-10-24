44bd61822bdfbccd873eab87b6f09d598ba8fa6a
sang0 <sang0@61.84.225.58>
2021-10-24 20:58:57 +0900

    랭킹수정

--- Java_miniproject_꽉자바/src/Javaproject/DAMADAO.java
+++ Java_miniproject_꽉자바/src/Javaproject/DAMADAO.java
@@ -210,7 +210,7 @@ public class DAMADAO {
 	public void ranksys() {
 		getConn();
 		int i = 0;
-		String sql = "select * from dama_info order by ES,LV asc";
+		String sql = "select * from dama_info order by LV desc";
 		try {
 			psmt = conn.prepareStatement(sql);
 			rs = psmt.executeQuery();
