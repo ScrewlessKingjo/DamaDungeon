429956b49bdda2a0ade6e4e16eb56bfe34f307b4
smhrd <smhrd@DESKTOP-U6Q8TOE>
2021-10-25 16:34:30 +0900

    페이스 약초 수정

--- Java_miniproject_꽉자바/src/Javaproject/Face.java
+++ Java_miniproject_꽉자바/src/Javaproject/Face.java
@@ -57,6 +57,18 @@ public class Face {
 	
 	public void Face_Eat_Medi(String id) {
 		st=dama.vo_loding(id);
+		System.out.println("    내 이름은 " + st.getNick()+ " 약초를 먹지 우웩 쓰다 써 인생");
+		System.out.println("                ∧___∧    ");
+		System.out.println("       _____  　(⊙＾⊙ )_ & ");
+		System.out.println("      (_약초_)   ( __ _  )   ");
+		System.out.println("                 ˘  ˘ ˘˘   ");
+		System.out.println();
+		System.out.println("    HP  : "+st.getHp()+"\t ENE : "+st.getEne());
+		System.out.println("    ATK : "+st.getAtk()+"\t DEF : "+st.getShd()+"\t SPD : "+st.getSpd());
+		System.out.println("    Lv  : "+st.getLv()+"\t EXP : "+st.getExpe());
+	}
+	public void Face_No_Medi(String id) {
+		st=dama.vo_loding(id);
 		System.out.println("    내 이름은 " + st.getNick()+"       날짜 : "+st.getStartday());
 		System.out.println("                ∧__∧        ");
 		System.out.println("   _____       (    )       ");
