873674566fa5ed48028b87b84e04f2e35296a6ea
smhrd <smhrd@DESKTOP-1HHI0S3>
2021-10-25 10:32:13 +0900

    sick class 수정ㅈ ㅜㅇ

--- Java_miniproject_꽉자바/src/Javaproject/Sick.java
+++ Java_miniproject_꽉자바/src/Javaproject/Sick.java
@@ -1,8 +1,21 @@
 package Javaproject;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Sick {
 	
-	public void Sick(){
+	Scanner sc = new Scanner(System.in);
+	Random rd = new Random();
+	
+	DAMADAO dama = new DAMADAO();
+	Face fa = new Face();
+	DAMAVO st = new DAMAVO();
+	
+	public void Sick(String id){
+		
+		st=dama.vo_loding(id);
+		st.VO_day(id, 1);
 		
 	}
 	
