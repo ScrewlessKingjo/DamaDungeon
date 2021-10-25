d61142b95ab20b6554ba8bfc3c2a9a48e88a6a29
smhrd <smhrd@DESKTOP-1HHI0S3>
2021-10-25 15:06:12 +0900

    sick 클래스 수정

--- Java_miniproject_꽉자바/src/Javaproject/Sick.java
+++ Java_miniproject_꽉자바/src/Javaproject/Sick.java
@@ -16,23 +16,22 @@ public class Sick {
 	public void SSick(String id){
 		st=dama.vo_loding(id);
 		sickday = st.getSickday()+1;
-		for (int i =1; i<sickday ;i++) {
-				System.out.println("질병에 걸렸습니다. 모든 스텟이 " +i+"만큼 하락합니다.");
-				st.setStr(st.getStr()-i);
-				st.setDex(st.getDex()-i);
-				st.setWis(st.getWis()-i);
-				st.setLuk(st.getLuk()-i);
-				st.setAtk(st.getAtk()-i);
-				st.setShd(st.getShd()-i);
-				st.setSpd(st.getSpd()-i);
-				st.setHp(st.getHp()-i*10);
-				st.setEne(st.getEne()-i*10);
+		fa.Face_Sick(id);
+		System.out.println("질병에 걸렸습니다. 모든 스텟이 " +sickday+"만큼 하락합니다.");
+				st.setStr(st.getStr()-sickday);
+				st.setDex(st.getDex()-sickday);
+				st.setWis(st.getWis()-sickday);
+				st.setLuk(st.getLuk()-sickday);
+				st.setAtk(st.getAtk()-sickday);
+				st.setShd(st.getShd()-sickday);
+				st.setSpd(st.getSpd()-sickday);
+				st.setHp(st.getHp()-sickday*10);
+				st.setEne(st.getEne()-sickday*10);
 				if(st.getHp() < 1) {
 					st.VO_deadly(id);
 				}
-				st.setSickday(i);
+				st.setSickday(sickday);
 				st.Alldate(id);
-				fa.Face_Sick(id);
-			}
+			
 		}
 }
